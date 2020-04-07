76fe9dcacde889820481dc58dc10375755aded5a
heretodesign <senwelo.works@gmail.com>
2020-04-08 01:22:35 +0800

    added normalize css and created the main card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import './normalize.css'
 
 function App() {
   return (
     <div>
-    
+      <header>
+        <h1>Study <span className="titleHighlight">Deck</span></h1>
+        <h2>Retention through repetition</h2>
+      </header>
+      <main>
+        <h3>Your Cards</h3>
+        <div className="gridContainer">
+          <div className="title">
+            <h4 className="cardTerm">Term Goes Here</h4>
+            <div className="cardButtons">
+              <button className="tertiary" type="button">show button</button>
+              <div>
+                <button className="secondary" type="button">edit button</button>
+                <button className="secondary danger" type="button">delete button</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
